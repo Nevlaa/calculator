f2c0e067ee6139469476deb63155876431ff7bdc
Scott horsburgh <shorsburgh19@gmail.com>
2020-09-12 23:20:57 -0600

    adding files and need to do some styling

---
+++ Scott_Horsburgh2.hwk/Calculator.Designer.cs
@@ -0,0 +1,173 @@
+﻿namespace Scott_Horsburgh2.hwk
+{
+    partial class Calculator
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.lblTitle = new System.Windows.Forms.Label();
+            this.lblCaption1 = new System.Windows.Forms.Label();
+            this.lblCaption2 = new System.Windows.Forms.Label();
+            this.txtValue1 = new System.Windows.Forms.TextBox();
+            this.txtValue2 = new System.Windows.Forms.TextBox();
+            this.BtnMultiply = new System.Windows.Forms.Button();
+            this.BtnAdd = new System.Windows.Forms.Button();
+            this.BtnReset = new System.Windows.Forms.Button();
+            this.lblResult = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // lblTitle
+            // 
+            this.lblTitle.AutoSize = true;
+            this.lblTitle.Font = new System.Drawing.Font("Microsoft Sans Serif", 16F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblTitle.Location = new System.Drawing.Point(269, 53);
+            this.lblTitle.Name = "lblTitle";
+            this.lblTitle.Size = new System.Drawing.Size(155, 26);
+            this.lblTitle.TabIndex = 0;
+            this.lblTitle.Text = "Calculator App";
+            // 
+            // lblCaption1
+            // 
+            this.lblCaption1.AutoSize = true;
+            this.lblCaption1.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblCaption1.Location = new System.Drawing.Point(61, 195);
+            this.lblCaption1.Name = "lblCaption1";
+            this.lblCaption1.Size = new System.Drawing.Size(85, 20);
+            this.lblCaption1.TabIndex = 1;
+            this.lblCaption1.Text = "First Value";
+            // 
+            // lblCaption2
+            // 
+            this.lblCaption2.AutoSize = true;
+            this.lblCaption2.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblCaption2.Location = new System.Drawing.Point(61, 291);
+            this.lblCaption2.Name = "lblCaption2";
+            this.lblCaption2.Size = new System.Drawing.Size(51, 20);
+            this.lblCaption2.TabIndex = 2;
+            this.lblCaption2.Text = "label2";
+            // 
+            // txtValue1
+            // 
+            this.txtValue1.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.txtValue1.Location = new System.Drawing.Point(307, 189);
+            this.txtValue1.Name = "txtValue1";
+            this.txtValue1.Size = new System.Drawing.Size(167, 26);
+            this.txtValue1.TabIndex = 3;
+            // 
+            // txtValue2
+            // 
+            this.txtValue2.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.txtValue2.Location = new System.Drawing.Point(307, 285);
+            this.txtValue2.Name = "txtValue2";
+            this.txtValue2.Size = new System.Drawing.Size(167, 26);
+            this.txtValue2.TabIndex = 4;
+            // 
+            // BtnMultiply
+            // 
+            this.BtnMultiply.Font = new System.Drawing.Font("Microsoft Sans Serif", 14F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.BtnMultiply.Location = new System.Drawing.Point(158, 398);
+            this.BtnMultiply.Margin = new System.Windows.Forms.Padding(2);
+            this.BtnMultiply.Name = "BtnMultiply";
+            this.BtnMultiply.Size = new System.Drawing.Size(92, 38);
+            this.BtnMultiply.TabIndex = 5;
+            this.BtnMultiply.Text = "Multiply";
+            this.BtnMultiply.UseVisualStyleBackColor = true;
+            this.BtnMultiply.Click += new System.EventHandler(this.BtnMultiply_Click);
+            // 
+            // BtnAdd
+            // 
+            this.BtnAdd.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.BtnAdd.Location = new System.Drawing.Point(438, 398);
+            this.BtnAdd.Margin = new System.Windows.Forms.Padding(2);
+            this.BtnAdd.Name = "BtnAdd";
+            this.BtnAdd.Size = new System.Drawing.Size(92, 38);
+            this.BtnAdd.TabIndex = 6;
+            this.BtnAdd.Text = "Add";
+            this.BtnAdd.UseVisualStyleBackColor = true;
+            this.BtnAdd.Click += new System.EventHandler(this.BtnAdd_Click);
+            // 
+            // BtnReset
+            // 
+            this.BtnReset.Font = new System.Drawing.Font("Microsoft Sans Serif", 14F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.BtnReset.Location = new System.Drawing.Point(307, 489);
+            this.BtnReset.Margin = new System.Windows.Forms.Padding(2);
+            this.BtnReset.Name = "BtnReset";
+            this.BtnReset.Size = new System.Drawing.Size(92, 38);
+            this.BtnReset.TabIndex = 7;
+            this.BtnReset.Text = "Reset";
+            this.BtnReset.UseVisualStyleBackColor = true;
+            this.BtnReset.Click += new System.EventHandler(this.BtnReset_Click);
+            // 
+            // lblResult
+            // 
+            this.lblResult.AutoSize = true;
+            this.lblResult.Font = new System.Drawing.Font("Microsoft Sans Serif", 14F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblResult.ForeColor = System.Drawing.SystemColors.HotTrack;
+            this.lblResult.Location = new System.Drawing.Point(64, 586);
+            this.lblResult.Margin = new System.Windows.Forms.Padding(2, 0, 2, 0);
+            this.lblResult.Name = "lblResult";
+            this.lblResult.Size = new System.Drawing.Size(60, 24);
+            this.lblResult.TabIndex = 8;
+            this.lblResult.Text = "label1";
+            this.lblResult.Visible = false;
+            // 
+            // Calculator
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.SystemColors.ButtonShadow;
+            this.ClientSize = new System.Drawing.Size(720, 624);
+            this.Controls.Add(this.lblResult);
+            this.Controls.Add(this.BtnReset);
+            this.Controls.Add(this.BtnAdd);
+            this.Controls.Add(this.BtnMultiply);
+            this.Controls.Add(this.txtValue2);
+            this.Controls.Add(this.txtValue1);
+            this.Controls.Add(this.lblCaption2);
+            this.Controls.Add(this.lblCaption1);
+            this.Controls.Add(this.lblTitle);
+            this.Font = new System.Drawing.Font("Microsoft Sans Serif", 8.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.Name = "Calculator";
+            this.Text = "First Value";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label lblTitle;
+        private System.Windows.Forms.Label lblCaption1;
+        private System.Windows.Forms.Label lblCaption2;
+        private System.Windows.Forms.TextBox txtValue1;
+        private System.Windows.Forms.TextBox txtValue2;
+        private System.Windows.Forms.Button BtnMultiply;
+        private System.Windows.Forms.Button BtnAdd;
+        private System.Windows.Forms.Button BtnReset;
+        private System.Windows.Forms.Label lblResult;
+    }
+}
+
---
+++ Scott_Horsburgh2.hwk/Calculator.cs
@@ -0,0 +1,94 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Scott_Horsburgh2.hwk
+{
+    public partial class Calculator : Form
+    {
+        public Calculator()
+        {
+            InitializeComponent();
+        }
+
+        private void BtnMultiply_Click(object sender, EventArgs e)
+        {
+            double val1, val2;
+
+            val1 = GetFirstValue();
+            val2 = GetSecondValue();
+
+            if(val1 > 0 && val2 > 0)
+            {
+                lblResult.ForeColor = Color.Green;
+                lblResult.Text = "Multiply Results  " + (val1 * val2).ToString();
+            }
+            lblResult.Visible = true;
+
+
+
+
+        }
+
+        private void BtnAdd_Click(object sender, EventArgs e)
+        {
+            double val1, val2;
+
+            val1 = GetFirstValue();
+            val2 = GetSecondValue();
+
+            if (val1 > 0 && val2 > 0)
+            {
+                lblResult.ForeColor = Color.Green;
+                lblResult.Text = "Add Results  " + (val1 + val2).ToString();
+            }
+            lblResult.Visible = true;
+
+        }
+
+        private void BtnReset_Click(object sender, EventArgs e)
+        {
+            txtValue1.Text = "";
+            txtValue2.Text = "";
+            lblResult.Visible = false;
+            txtValue1.Focus();
+
+        }
+
+        public double GetFirstValue()
+        {
+            double val1;
+
+            while (double.TryParse(txtValue1.Text, out val1) == false)
+            {
+                lblResult.ForeColor = Color.Red;
+                lblResult.Text = "Value must be numeric and > 0.";
+                lblResult.Visible = true;
+                txtValue1.Text = "0";
+                txtValue1.Focus();
+            }
+            return val1;
+        }
+
+        public double GetSecondValue()
+        {
+            double val2;
+
+            while (double.TryParse(txtValue1.Text, out val2) == false)
+            {
+                lblResult.ForeColor = Color.Red;
+                lblResult.Text = "Value must be numeric and > 0.";
+                lblResult.Visible = true;
+                txtValue2.Text = "0";
+                txtValue2.Focus();
+            }
+            return val2;
+        }
+    }
+}
